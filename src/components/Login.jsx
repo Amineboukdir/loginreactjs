bd915a230673d6dac36c24771f1fe2ec67e4626c
Amine boukdir <amine.boukdir@hps-worldwide.com>
2024-04-09 06:14:40 +0100

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import Image from "../assets/aia.png";
+import Logo from "../assets/hps.png";
+import GoogleSvg from "../assets/icons8-google.svg";
+import { FaEye } from "react-icons/fa6";
+import { FaEyeSlash } from "react-icons/fa6";
+
+
+
+const Login = () => {
+  const [ showPassword, setShowPassword ] = useState(false);
+
+
+  return (
+    <div className="login-main">
+     
+
+      <div className="login-left">
+        <img src={Image} alt="" />
+        
+        <ul class="nav flex-column flex-sm-row">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#">Disabled</a>
+  </li>
+</ul>
+
+
+
+      </div>
+      <div className="login-right">
+        <div className="login-right-container">
+          <div className="login-logo">
+            <img src={Logo} alt="" />
+          </div>
+          <div className="login-center">
+            <h2>Welcome back!</h2>
+            <p>Please enter your details</p>
+            <form>
+              <input type="email" placeholder="Email" />
+              <div className="pass-input-div">
+                <input type={showPassword ? "text" : "password"} placeholder="Password" />
+                {showPassword ? <FaEyeSlash onClick={() => {setShowPassword(!showPassword)}} /> : <FaEye onClick={() => {setShowPassword(!showPassword)}} />}
+                
+              </div>
+
+              <div className="login-center-options">
+                <div className="remember-div">
+                  <input type="checkbox" id="remember-checkbox" />
+                  <label htmlFor="remember-checkbox">
+                    Remember for 30 days
+                  </label>
+                </div>
+                <a href="#" className="forgot-pass-link">
+                  Forgot password?
+                </a>
+              </div>
+              <div className="login-center-buttons">
+                <button type="button">Log In</button>
+                <button type="button">
+                  <img src={GoogleSvg} alt="" />
+                  Log In with Google
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <p className="login-bottom-p">
+            Don't have an account? <a href="#">Sign Up</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
